1f6f025a3892d08e3c8b7a7ba9e1c1236dd0a38b
Igor Mandrichenko <ivm@mac-132508.dhcp.fnal.gov>
2019-09-26 11:23:05 -0500

    moved micro-server docker container config to externally mounted directory

--- docker/run_micro_server.sh
+++ docker/run_micro_server.sh
@@ -1,3 +1,57 @@
 #!/bin/sh
 
-docker run --rm -p 8766:8766 -p 8765:8765 striped_micro_server:latest
+Usage="run_micro_server.sh [-c <config>] [-i]\n"
+
+config=`pwd`/config
+cmd=""
+ti=""
+
+while [ "$1" != "" ]
+do
+	case "$1" in
+		-i)	
+        	ti="-ti"
+        	cmd=/bin/bash
+        	;;
+		-c)	
+            if [ ! -d $2 ]; then
+                echo $2 is invalid path
+                exit 1
+            fi
+            config=`cd $2; pwd`
+			shift
+			;;
+        -\?|-h|--help)
+            echo "Usage: run_micro_server.sh [-c <config>] [-i]"
+            echo "   -c <config> - absolute path to config directory. Default: $PWD/config"
+            echo "   -i - do not start Striped server. Instead, run interactive shell"
+            echo
+            exit 2
+            ;;
+	esac
+	shift
+done
+
+if [ ! -f ${config}/job_server.yaml ]; then
+    echo Error: ${config}/job_server.yaml file not found
+    exit 1
+fi 
+
+if [ ! -f ${config}/worker_master.yaml ]; then
+    echo Error: ${config}/worker_master.yaml file not found
+    exit 1
+fi 
+
+echo will use $config as the configuration directory
+
+if [ "$ti" == "" ]; then
+    echo starting the Striped server
+else 
+    echo starting interactive shell instead of the Striped server    
+fi
+
+docker run --rm \
+	-p 8766:8766 -p 8765:8765 \
+	--mount type=bind,source=${config},target=/home/striped/striped_home/config \
+	$ti \
+	striped_micro_server:latest $cmd
